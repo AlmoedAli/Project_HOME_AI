d5026ad58ec25a5bed823447dcd2a814da3c6b7e
Tho Nguyen Xuan <tho.nguyenxuantho573@hcmut.edu.vn>
2024-04-04 09:39:44 +0700

    Change directory of SVG folder

--- src/helpers/readSVG.js
+++ src/helpers/readSVG.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 function readSVG(iconName) {
-	let dir = path.join(__dirname, "/..", "assets", "svg", iconName + ".svg");
+	let dir = path.join(__dirname, "/..", "public", "svg", iconName + ".svg");
 	return fs.readFileSync(dir, "utf8");
 }
 
