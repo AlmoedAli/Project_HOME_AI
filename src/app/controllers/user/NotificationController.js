8242d7f1145b758b808b99f95070c556a50d7529
Nguyễn Thái Tân <nguyenthaitan02@gmail.com>
2024-05-03 19:45:34 +0700

    notification view

--- src/app/controllers/user/NotificationController.js
+++ src/app/controllers/user/NotificationController.js
@@ -1,4 +1,7 @@
 const notification = require("../../models/notification");
+const device = require("../../models/device");
+const sensor = require("../../models/sensor");
+const equipment = require("../../models/equipment");
 const { ObjectId } = require('mongodb');
 
 class NotificationController {
@@ -16,6 +19,37 @@ class NotificationController {
             notifications: notifications
         })
     }
+
+    async getNotification(req, res, next) {
+		try {
+			const noti = (await notification.findById(req.params.id)).toObject();
+            const dev = (await device.findById(noti.DeviceID)).toObject();
+            const sen = await sensor.findOne({DeviceID: noti.DeviceID});
+            const equip = await equipment.findOne({DeviceID: noti.DeviceID});
+            let result;
+            if (sen === null) {
+                result = await(equip.toObject());
+                res.render("user/notification_detail_equipment", {
+                    layout: "main",
+                    noti: noti,
+                    dev: dev,
+                    detail: result,
+                });
+            }
+            else {
+                result = await(sen.toObject());
+                res.render("user/notification_detail_sensor", {
+                    layout: "main",
+                    noti: noti,
+                    dev: dev,
+                    detail: result,
+                });
+            }
+
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 module.exports = new NotificationController();
--- src/routes/user/notification.js
+++ src/routes/user/notification.js
@@ -4,7 +4,7 @@ const { isAuthenticated } = require("../../middlewares/session");
 const notificationController = require("../../app/controllers/user/NotificationController");
 
 router.get("/", notificationController.index);
-// router.get("/:id", notificationController.getNotification);
+router.get("/:id", notificationController.getNotification);
 // router.get("/:id/edit", notificationController.index_modify);
 // router.post("/:id/edit", notificationController.notification_modify);
 // router.delete("/delete", notificationController.deleteAllNotifications);
