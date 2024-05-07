0bad3702669d53d9ee02c1d39e2f6ac4300257ad
Quan Nguyen <119392873+QuanNguyen-1st@users.noreply.github.com>
2024-05-07 10:27:49 +0700

    +add authentication

--- src/routes/user/equipment.js
+++ src/routes/user/equipment.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { isAuthenticated } = require("../../middlewares/session");
 const equipmentController = require("../../app/controllers/user/EquipmentController");
 
-router.get("/", equipmentController.index);
-router.get("/:id", equipmentController.getEquipment);
-router.get("/:id/edit", equipmentController.index_modify);
-router.post("/:id/edit", equipmentController.equipment_modify);
-router.delete("/delete", equipmentController.deleteAllEquipments);
+router.get("/", isAuthenticated, equipmentController.index);
+router.get("/:id", isAuthenticated, equipmentController.getEquipment);
+router.get("/:id/edit", isAuthenticated, equipmentController.index_modify);
+router.post("/:id/edit", isAuthenticated, equipmentController.equipment_modify);
+router.delete("/delete", isAuthenticated, equipmentController.deleteAllEquipments);
 module.exports = router;
     
