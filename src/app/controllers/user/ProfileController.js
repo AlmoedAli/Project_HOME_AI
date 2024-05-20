b144da458646d72312301954a343cb978ea24c2c
Nguyễn Xuân Thọ <xuantho573@gmail.com>
2024-05-20 16:06:13 +0700

    remove voice section from profile view

--- src/app/controllers/user/ProfileController.js
+++ src/app/controllers/user/ProfileController.js
@@ -1,33 +1,16 @@
-const voice = require("../../models/voice");
-const { unlink } = require("fs");
+const house = require("../../models/house");
 
 class ProfileController {
 	async index(req, res) {
 		const user = req.session.user;
-		const result = await voice.findOne({ userID: user._id });
+		const houses = (await house.find({ UserID: user._id })).map((h) => {
+			return { name: h.Name, address: h.Address };
+		});
 		res.render("user/profile", {
 			layout: "main",
 			user: user,
-			voicePath: result?.filePath,
+			houses: houses,
 		});
-	}
-
-	async addVoice(req, res) {
-		const result = await voice.create({
-			userID: req.session.user._id,
-			filePath: `sounds/samples/${req.file.filename}`,
-		});
-		res.status(201).json(result);
-	}
-
-	async removeVoice(req, res) {
-		const result = await voice.findOneAndDelete({
-			userID: req.session.user._id,
-		});
-		unlink(`src/public/${result.filePath}`, (err) => {
-			if (err) throw err;
-		});
-		res.status(200).json(result);
 	}
 }
 
